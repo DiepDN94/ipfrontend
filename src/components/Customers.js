bce3f75642e9c607627394334eaf8fc55fc3d2eb
Diep Nguyen <144938188+DiepDN94@users.noreply.github.com>
2023-10-10 19:52:57 -0400

    Update Customers.js

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -7,11 +7,56 @@ const Customers = () => {
   const [customers, setCustomers] = useState([]);
   const [searchText, setSearchText] = useState('');
 
-  useEffect(() => {
-    fetchCustomers(searchText);
-  }, [searchText]);
+  useEffect(() => {}, []);
 
+  const handleValueChange = (newValue, customerId, fieldName) => {
+    setCustomers(prevCustomers => {
+        return prevCustomers.map(customer => {
+            if (customer.customer_id === customerId) {
+                return { ...customer, [fieldName]: newValue };
+            } else {
+                return customer;
+            }
+        });
+    });
+  };
 
+  const EditableCell = ({ value, onValueChange, fieldName, customerId }) => {
+    const [inputValue, setInputValue] = useState(value);
+  
+    const handleInputChange = (e) => {
+      setInputValue(e.target.value);
+    };
+  
+    const handleUpdateDatabase = async () => {
+      onValueChange(inputValue, customerId, fieldName); 
+      try {
+        await api.post('/updateCustomerDetails', {
+          customerId,
+          fieldName,
+          newValue: inputValue
+        });
+      } catch (err) {
+        console.error("Error updating customer details:", err);
+      }
+    };
+  
+    return (
+      <input 
+        type="text" 
+        value={inputValue} 
+        onChange={handleInputChange}
+        onBlur={handleUpdateDatabase}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault();
+            handleUpdateDatabase();
+          }
+        }}
+      />
+    );
+  };
+  
   const fetchCustomers = async (search) => {
     try {
       const response = await api.get(`/getCustomer?search=${search}`);
@@ -24,6 +69,20 @@ const Customers = () => {
   const handleSearch = () => {
     fetchCustomers(searchText);
   };
+  
+  const handleDelete = async (customerId) => {
+    try {
+        const response = await api.post('/deleteCustomer', { customerId });
+        if (response.data.success) {
+            setCustomers(customers.filter(c => c.customer_id !== customerId));
+        } else {
+            alert(response.data.message || 'Failed to delete customer.');
+        }
+    } catch (err) {
+        console.error("Error deleting customer:", err);
+        alert('Error deleting customer.');
+    }
+  };
 
   const openAddCustomerForm = () => {
     const newWindow = window.open("", "Add Customer", "width=600,height=600");
@@ -33,7 +92,7 @@ const Customers = () => {
     newWindow.document.close();
     ReactDOM.render(<AddCustomer />, newWindow.document.getElementById("root"));
   }
-
+  
   return (
     <div>
 
@@ -45,6 +104,7 @@ const Customers = () => {
           <input
             type="text"
             value={searchText}
+            placeholder="Enter first, last, or email" 
             onChange={(e) => setSearchText(e.target.value)}
           />
         </label>
@@ -54,11 +114,16 @@ const Customers = () => {
       <table>
         <thead>
           <tr>
-            <th>Customer ID</th>
+            <th>ID</th>
             <th>First Name</th>
             <th>Last Name</th>
             <th>Email</th>
             <th>Address</th>
+            <th>City</th>
+            <th>District</th> 
+            <th>Country</th>
+            <th>Phone</th>
+            <th>Action</th>
           </tr>
         </thead>
 
@@ -66,13 +131,19 @@ const Customers = () => {
           {customers.map((customer) => (
             <tr key={customer.customer_id}>
               <td>{customer.customer_id}</td>
-              <td>{customer.first_name}</td>
-              <td>{customer.last_name}</td>
-              <td>{customer.email}</td>
-              <td>{customer.address}</td>
+              <td><EditableCell value={customer.first_name} fieldName="first_name" customerId={customer.customer_id} onValueChange={handleValueChange} /></td>
+              <td><EditableCell value={customer.last_name} fieldName="last_name" customerId={customer.customer_id} onValueChange={handleValueChange} /></td>
+              <td><EditableCell value={customer.email} fieldName="email" customerId={customer.customer_id} onValueChange={handleValueChange} /></td>
+              <td><EditableCell value={customer.address} fieldName="address" customerId={customer.customer_id} onValueChange={handleValueChange} /></td>
+              <td><EditableCell value={customer.district} fieldName="district" customerId={customer.customer_id} onValueChange={handleValueChange} /></td>
+              <td><EditableCell value={customer.city} fieldName="city" customerId={customer.customer_id} onValueChange={handleValueChange} /></td>
+              <td><EditableCell value={customer.country} fieldName="country" customerId={customer.customer_id} onValueChange={handleValueChange} /></td>
+              <td><EditableCell value={customer.phone} fieldName="phone" customerId={customer.customer_id} onValueChange={handleValueChange} /></td>
+              <td><button onClick={() => handleDelete(customer.customer_id)}>Delete</button></td>
             </tr>
           ))}
         </tbody>
+
       </table>
 
     </div>
